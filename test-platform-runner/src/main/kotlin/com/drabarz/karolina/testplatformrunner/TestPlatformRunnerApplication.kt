c1114a4f5a8199cfe02f12542eed89a8d50cd736
Karolina Drabarz <kdr@touk.pl>
2019-04-08 21:13:17 +0200

    Init test-platform-runner

---
+++ test-platform-runner/src/main/kotlin/com/drabarz/karolina/testplatformrunner/TestPlatformRunnerApplication.kt
@@ -0,0 +1,87 @@
+package com.drabarz.karolina.testplatformrunner
+
+import org.hibernate.annotations.common.util.impl.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
+import org.testcontainers.containers.BindMode
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.output.OutputFrame
+import org.testcontainers.containers.output.ToStringConsumer
+import org.testcontainers.images.builder.ImageFromDockerfile
+import org.testcontainers.utility.MountableFile
+
+
+@SpringBootApplication
+class TestPlatformRunnerApplication
+
+fun main(args: Array<String>) {
+	TestPlatformRunnerApplication::class.java.getResource("/static/Dockerfile").readText().let{print(it)}
+	runApplication<TestPlatformRunnerApplication>(*args)
+}
+
+@RestController
+class TestPlatformApi {
+
+	val jarService = JarService()
+
+	@PostMapping("/jar")
+	fun uploadJar(@RequestParam("file") uploadedFile: MultipartFile): String {
+		val originalFileName = uploadedFile.originalFilename
+
+		jarService.saveFile(uploadedFile)
+
+		jarService.runJar(originalFileName)
+
+		return "200"
+	}
+}
+
+@Component
+class JarService {
+
+	fun saveFile(uploadedFile: MultipartFile) {
+		val outputFile = File(PATH_PREFIX, uploadedFile.originalFilename)
+		uploadedFile.transferTo(outputFile)
+	}
+
+	fun runJar(originalFileName: String?) {
+		val log = LoggerFactory.logger(this.javaClass)
+
+		val container = KGenericContainer(
+				ImageFromDockerfile()
+						.withFileFromClasspath("Dockerfile", "static/Dockerfile")
+		)
+				.withCopyFileToContainer(MountableFile.forHostPath(PATH_PREFIX + "/" + originalFileName), "/home/example.jar")
+				.withClasspathResourceMapping("static/input.txt", "/home/input.txt", BindMode.READ_ONLY)
+				.withClasspathResourceMapping("static/output.txt", "/home/output.txt", BindMode.READ_WRITE)
+
+		container.start()
+
+
+		Thread.sleep(1000)
+
+		val toStringConsumer = ToStringConsumer()
+		container.followOutput(toStringConsumer, OutputFrame.OutputType.STDERR)
+
+		log.info(container.containerId)
+		log.info(toStringConsumer.toUtf8String())
+		log.info(container.logs)
+
+		container.stop()
+
+		JarService::class.java.getResource("/static/output.txt").readText().let { log.info(it) }
+	}
+
+	companion object {
+	    val PATH_PREFIX = "/home/karolina/MGR/tests";
+	}
+}
+
+class KGenericContainer(imageName: ImageFromDockerfile) : GenericContainer<KGenericContainer>(imageName)
+
---
+++ test-platform-runner/src/test/kotlin/com/drabarz/karolina/testplatformrunner/TestPlatformRunnerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.drabarz.karolina.testplatformrunner
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TestPlatformRunnerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
