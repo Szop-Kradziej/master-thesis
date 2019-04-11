cc60609b6a646441dbab12878df6325c1c07717d
Karolina Drabarz <kdr@touk.pl>
2019-04-11 15:54:47 +0200

    Add new project from FE

--- fe-test-platform-runner/src/ProjectsBoard.js
+++ fe-test-platform-runner/src/ProjectsBoard.js
@@ -6,19 +6,22 @@ import TableHead from "@material-ui/core/TableHead/TableHead";
 import TableRow from "@material-ui/core/TableRow/TableRow";
 import TableBody from "@material-ui/core/TableBody/TableBody";
 import TableCell from "@material-ui/core/TableCell/TableCell";
+import Button from "@material-ui/core/Button/Button";
+import Modal from "@material-ui/core/Modal/Modal";
+import TextField from "@material-ui/core/TextField/TextField";
 
 class ProjectsBoard extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {projects: {projects: []}};
+        this.state = {projects: {projects: []}, isNewProjectDialogVisible: false, newProjectName: null};
     }
 
     componentDidMount() {
-        this.fetchScores();
+        this.fetchProjects();
     }
 
-    fetchScores = () => {
+    fetchProjects = () => {
         fetch(backendUrl(`/projects`), {
             method: "GET",
             credentials: "include"
@@ -27,6 +30,29 @@ class ProjectsBoard extends React.Component {
             .then(json => this.setState({
                 projects: json
             }))
+    };
+
+    handleOpenNewProjectDialog = () => {
+        this.setState({isNewProjectDialogVisible: true});
+    };
+
+    handleCloseNewProjectDialog = () => {
+        this.setState({isNewProjectDialogVisible: false});
+    };
+
+    handleNewProjectNameAdded = () => event => {
+        this.setState({newProjectName: event.target.value})
+    };
+
+    handleAddNewProject = () => {
+        fetch(backendUrl(`/project`), {
+            method: "POST",
+            credentials: "include",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: "projectName=" + this.state.newProjectName
+        })
+            .then(() => this.setState({isNewProjectDialogVisible: false, newProjectName: null}))
+            .then(this.fetchProjects)
     };
 
     render() {
@@ -48,7 +74,30 @@ class ProjectsBoard extends React.Component {
                         ))}
                     </TableBody>
                 </Table>
+                <Button className={this.props.classes.button} onClick={this.handleOpenNewProjectDialog}>
+                    Dodaj nowy projekt
+                </Button>
 
+                <Modal open={this.state.isNewProjectDialogVisible}>
+                    <div style={getModalStyle()} className={this.props.classes.paper}>
+                        <form className={this.props.classes.container} noValidate autoComplete="off">
+                            <TextField
+                                id="standard-name"
+                                label="Nazwa projektu"
+                                className={this.props.classes.textField}
+                                value={this.state.newProjectName}
+                                onChange={this.handleNewProjectNameAdded()}
+                                margin="normal"
+                            />
+                            <Button className={this.props.classes.button} onClick={this.handleAddNewProject}>
+                                Dodaj
+                            </Button>
+                            {/*<Button className={this.props.classes.button} onClick={this.handleCloseNewProjectDialog()}>*/}
+                            {/*Anuluj*/}
+                            {/*</Button>*/}
+                        </form>
+                    </div>
+                </Modal>
             </div>
         );
     }
@@ -66,7 +115,7 @@ const CustomTableCell = withStyles(() => ({
     }
 }))(TableCell);
 
-const styles = () => ({
+const styles = (theme) => ({
     app: {
         textAlign: "center",
         backgroundColor: "#e0e0e0",
@@ -81,7 +130,40 @@ const styles = () => ({
         minWidth: 900,
         fontSize: 16,
         color: "black"
-    }
+    },
+    button: {
+        backgroundColor: "#5aa724",
+        color: "black",
+        marginTop: 20
+    },
+    paper: {
+        position: 'absolute',
+        width: theme.spacing.unit * 50,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing.unit * 4,
+        outline: 'none',
+    },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
 });
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
 
 export default withStyles(styles)(ProjectsBoard)
