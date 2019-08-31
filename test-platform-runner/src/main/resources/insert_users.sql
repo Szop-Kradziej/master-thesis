4a1f17e068ab8e8234b9c0158f1e02d50efe73f9
Karolina Drabarz <kdr@touk.pl>
2019-08-31 14:30:31 +0200

    Add users insert file

---
+++ test-platform-runner/src/main/resources/insert_users.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (name, is_student)
+VALUES ('chcialemmiecinnylogin@gmail.com', true);
+INSERT INTO users (name, is_student)
+VALUES ('ordon1313@gmail.com', false);
+INSERT INTO users (name, is_student)
+VALUES ('karolina.drabarz@gmail.com', false);
+INSERT INTO users (name, is_student)
+VALUES ('kghost0@gmail.com', true);
+INSERT INTO users (name, is_student)
+VALUES ('kasper.kondzielski@gmail.com', true);
