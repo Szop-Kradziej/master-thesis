f4e3bf86398ab78fecdb8afbcc3a69e4f4f13540
Karolina Drabarz <kdr@touk.pl>
2019-08-31 21:33:35 +0200

    Fix redirection login button

--- fe-test-platform-runner/src/Login.js
+++ fe-test-platform-runner/src/Login.js
@@ -6,32 +6,31 @@ import {styles} from "./styles/AppStyles";
 class Login extends Component {
 
     loginHandler = () => {
-      console.log("Login")
+        console.log("Login");
+        window.location.href = "https://github.com/login/oauth/authorize?scope=user:email&client_id=e984173171ad3f4c4133";
     };
 
-  render() {
-    return (
-        <div className={this.props.classes.app}>
-            <header className={this.props.classes.header}>
-                <p>
-                    Panel logowania
-                </p>
-            </header>
-            <body className={this.props.classes.body}>
-                    <Button
-                        size="large"
-                        className={this.props.classes.button}
-                        variant="contained"
-                        onClick={this.loginHandler}>
-                        <a href={"https://github.com/login/oauth/authorize?scope=user:email&client_id=e984173171ad3f4c4133"}>
-                            Zaloguj
-                        </a>
-                    </Button>
-            </body>
+    render() {
+        return (
+            <div className={this.props.classes.app}>
+                <header className={this.props.classes.header}>
+                    <p>
+                        Panel logowania
+                    </p>
+                </header>
+                <body className={this.props.classes.body}>
+                <Button
+                    size="large"
+                    className={this.props.classes.button}
+                    variant="contained"
+                    onClick={this.loginHandler}>
+                    Zaloguj
+                </Button>
+                </body>
 
-        </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default withStyles(styles)(Login);
